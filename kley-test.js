a7e3c06d0ae3a29fe37da10b8f208c2d3131cfae
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 16:23:56 +0800

    Updated test.

--- kley-test.js
+++ kley-test.js
@@ -2,6 +2,6 @@ const assert = require( "assert" );
 const kley = require( "./kley.js" );
 
 assert.deepEqual( kley( { "hello": "world" }, "yeah", [ "ugh" ], { "hello": true } ),
-						[ 'world', 'yeah', 'ugh', 'hello' ], "should be deeply equal" );
+	[ "world", "yeah", "ugh", "hello" ], "should be equal to [ 'world', 'yeah', 'ugh', 'hello' ]" );
 
 console.log( "ok" );
