6da99802ec8242733e6b6ced858835ba2f3e560e
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-29 14:13:08 +0800

    Update client/server unit test.

--- kley-test.js
+++
@@ -1,7 +0,0 @@
-const assert = require( "assert" );
-const kley = require( "./kley.js" );
-
-assert.deepEqual( kley( { "hello": "world" }, "yeah", [ "ugh" ], { "hello": true } ),
-	[ "world", "yeah", "ugh", "hello" ], "should be equal to [ 'world', 'yeah', 'ugh', 'hello' ]" );
-
-console.log( "ok" );
